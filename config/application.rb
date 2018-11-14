33165fc085bccf6c7f7f11cee9bc2b13d3a34e5e
EC2 Default User <ec2-user@ip-172-31-5-93.us-east-2.compute.internal>
2018-11-14 07:34:09 +0000

    yarn attempt

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,9 @@ module AIUBlog
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-    
+config.assets.precompile += Ckeditor.assets
+config.assets.precompile += %w(ckeditor/* )
+config.autoload_paths += %W("#{config.root}/app/models/ckeditor")
 
   end
 end
